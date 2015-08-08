a7a82b1b90c3b235f1fcf22f647d098d08be6e0e
Astika <astikagupta@gmail.com>
2015-08-07 17:23:34 -0700

    ex00 done

--- HW04_ex00.py
+++ HW04_ex00.py
@@ -11,18 +11,41 @@
 #         - then ends the program
 ################################################################################
 # Imports
-
+import random
 
 # Body
 
-
+def guess():
+ 
+ x=random.randint(1,25)
+ print x
+ i=1
+ for i in range(5):
+  
+  print "Please guess a no"
+  s=raw_input()
+  try:
+   s_int=int(s)
+  except:
+   print "not a number. Try again."
+  else: 
+   if s_int==x:
+    print "you guessed correctly!"
+    break
+   elif s_int<x:
+    print "too low"
+    continue
+   else:
+    print "too high"
+    continue
+   print "you get just 5 chances. Bye!"
 
 
 ################################################################################
 def main():
 
 
-    print("Hello World!") # Remove this and replace with your function calls
+   guess()
     
 
 if __name__ == '__main__':
